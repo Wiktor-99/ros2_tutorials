aeb8f0e8692b2406121671cb6a1d6a4ef0d4bae9
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-05-20 22:21:55 +0200

    Add pkg info for simple_bot_joint_state_publisher

--- src/simple_bot_joint_state_publisher/setup.py
+++ src/simple_bot_joint_state_publisher/setup.py
@@ -4,7 +4,7 @@ package_name = 'simple_bot_joint_state_publisher'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ros',
+    maintainer='wiktorbajor1@gmail.com',
     maintainer_email='wiktorbajor1@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Simple one joint joint_states publisher.',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
