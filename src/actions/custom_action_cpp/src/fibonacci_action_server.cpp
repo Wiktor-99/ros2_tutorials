6392a6db9927440774a00680a57c98ac00f17700
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-05-24 22:26:08 +0200

    Change void cast to maybe_unused and remove useless comments

--- src/actions/custom_action_cpp/src/fibonacci_action_server.cpp
+++ src/actions/custom_action_cpp/src/fibonacci_action_server.cpp
@@ -33,26 +33,23 @@ private:
   rclcpp_action::Server<Fibonacci>::SharedPtr action_server_;
 
   rclcpp_action::GoalResponse handle_goal(
-    const rclcpp_action::GoalUUID & uuid,
+    [[maybe_unused]] const rclcpp_action::GoalUUID & uuid,
     std::shared_ptr<const Fibonacci::Goal> goal)
   {
     RCLCPP_INFO(this->get_logger(), "Received goal request with order %d", goal->order);
-    (void)uuid;
     return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
   }
 
   rclcpp_action::CancelResponse handle_cancel(
-    const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+    [[maybe_unused]] const std::shared_ptr<GoalHandleFibonacci> goal_handle)
   {
     RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
-    (void)goal_handle;
     return rclcpp_action::CancelResponse::ACCEPT;
   }
 
   void handle_accepted(const std::shared_ptr<GoalHandleFibonacci> goal_handle)
   {
     using namespace std::placeholders;
-    // this needs to return quickly to avoid blocking the executor, so spin up a new thread
     std::thread{std::bind(&FibonacciActionServer::execute, this, _1), goal_handle}.detach();
   }
 
@@ -68,23 +65,18 @@ private:
     auto result = std::make_shared<Fibonacci::Result>();
 
     for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i) {
-      // Check if there is a cancel request
       if (goal_handle->is_canceling()) {
         result->sequence = sequence;
         goal_handle->canceled(result);
         RCLCPP_INFO(this->get_logger(), "Goal canceled");
         return;
       }
-      // Update sequence
       sequence.push_back(sequence[i] + sequence[i - 1]);
-      // Publish feedback
       goal_handle->publish_feedback(feedback);
       RCLCPP_INFO(this->get_logger(), "Publish feedback");
-
       loop_rate.sleep();
     }
 
-    // Check if goal is done
     if (rclcpp::ok()) {
       result->sequence = sequence;
       goal_handle->succeed(result);
